c149ab13ada4aaae707d228bc426bae9cf1f66fa
AdamTonge <adam.tonge@mydit.ie>
2017-07-14 15:16:50 +0200

    camera script

---
+++ Assets/ThirdPersonCamera.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    private const float Y_ANGLE_MIN = -50.0f;
+    private const float Y_ANGLE_MAX = 50.0f;
+
+    public Transform lookAt;
+    public Transform camTransform;
+
+    private Camera cam;
+
+    private float distance = 25.0f;
+    private float currentX = 35.0f;
+    private float currentY = 0.0f;
+    private float sensivityX = 4.0f;
+    private float sensivityY = 1.0f;
+
+
+
+    // Use this for initialization
+    private void Start ()
+    {
+        camTransform = transform;
+        cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentX, currentY, 0);
+        camTransform.position = lookAt.position + rotation * dir;
+        camTransform.LookAt(lookAt.position);
+    }
+}
